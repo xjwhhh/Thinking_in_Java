151f73fe89d1f100fda799660e94c340630f3135
xjwhhh <151250171@smail.nju.edu.cn>
2017-12-27 21:25:18 +0800

    ali recruitment spider

---
+++ src/spider/Ali.java
@@ -0,0 +1,104 @@
+package spider;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+public class Ali {
+
+   static HashMap<String, Recruitment> stringrecruitmentHashMap=new HashMap<>();
+
+    public void getJobLinks(){
+
+        try {
+            Document doc = Jsoup.connect("https://campus.alibaba.com/positionList.htm").get();
+
+            Elements jobList=doc.getElementsByClass("campus-position-table-body");
+            for(Element element:jobList){
+                Elements jobs=element.select("tr");
+                for(Element job:jobs){
+                    Element jobLink=job.selectFirst("th>a");
+                    stringrecruitmentHashMap.put(jobLink.attr("href"),new Recruitment());
+                }
+            }
+
+
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void getRecruitmentDetailInfo(){
+       for(String jobLink:stringrecruitmentHashMap.keySet()){
+           Recruitment recruitment=stringrecruitmentHashMap.get(jobLink);
+           try {
+               Document doc = Jsoup.connect(jobLink).get();
+               Elements content=doc.getElementsByClass("word word_image content");
+               Elements jobName=content.get(0).getElementsByTag("h5");
+               recruitment.setJobName(jobName.get(0).text());
+
+               Element element=doc.getElementById("J-jobs");
+               Element jobBasicInfo=element.selectFirst("dl");
+
+               String jobBasicInfoString=jobBasicInfo.text();
+               String[] jobInfo=jobBasicInfoString.split("岗位要求");
+
+               String jobDescription=jobInfo[0];
+               String jobRequirementInfo=jobInfo[1];
+
+               String[] jobRequirements=jobRequirementInfo.split("工作地点");
+               String jobRequirement=jobRequirements[0];
+
+               jobDescription=jobDescription.replace("岗位描述","");
+               jobDescription=jobDescription.replace("Job Description ","");
+
+               jobRequirement=jobRequirement.replace("Qualifications ","");
+
+
+
+               recruitment.setJobDescription(jobDescription);
+               recruitment.setJobRequirement(jobRequirement);
+
+               Element workAddress=element.selectFirst(".work-place");
+               recruitment.setWorkAddress(workAddress.text());
+
+
+           }catch (IOException e){
+               e.printStackTrace();
+           }
+       }
+
+
+    }
+
+    public void getTreatment(){
+
+    }
+
+    public void getCompanyIntroduction(){
+
+    }
+
+
+    public static void main(String[] args){
+        Ali test=new Ali();
+        test.getJobLinks();
+        test.getRecruitmentDetailInfo();
+
+        for(String jobLink:stringrecruitmentHashMap.keySet()){
+            Recruitment recruitment=stringrecruitmentHashMap.get(jobLink);
+            System.out.println("工作名称:"+recruitment.getJobName());
+            System.out.println("工作地点:"+recruitment.getWorkAddress());
+            System.out.println("岗位描述:"+recruitment.getJobDescription());
+            System.out.println("岗位要求:"+recruitment.getJobRequirement());
+            System.out.println("===================================================");
+
+        }
+
+    }
+}
---
+++ src/spider/Recruitment.java
@@ -0,0 +1,68 @@
+package spider;
+
+public class Recruitment {
+    private String companyIntroduction;
+    private String jobName;
+    private String recruit;//招聘对象
+    private String jobDescription;
+    private String jobRequirement;
+    private String workAddress;
+    private String treatment;
+
+    public String getCompanyIntroduction() {
+        return companyIntroduction;
+    }
+
+    public void setCompanyIntroduction(String companyIntroduction) {
+        this.companyIntroduction = companyIntroduction;
+    }
+
+    public String getJobName() {
+        return jobName;
+    }
+
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+
+    public String getRecruit() {
+        return recruit;
+    }
+
+    public void setRecruit(String recruit) {
+        this.recruit = recruit;
+    }
+
+    public String getJobDescription() {
+        return jobDescription;
+    }
+
+    public void setJobDescription(String jobDescription) {
+        this.jobDescription = jobDescription;
+    }
+
+    public String getJobRequirement() {
+        return jobRequirement;
+    }
+
+    public void setJobRequirement(String jobRequirement) {
+        this.jobRequirement = jobRequirement;
+    }
+
+    public String getWorkAddress() {
+        return workAddress;
+    }
+
+    public void setWorkAddress(String workAddress) {
+        this.workAddress = workAddress;
+    }
+
+    public String getTreatment() {
+        return treatment;
+    }
+
+    public void setTreatment(String treatment) {
+        this.treatment = treatment;
+    }
+
+}
