60a1b55960dcca7ef83343ba055d0f3922a8609a
xjwhhh <151250171@smail.nju.edu.cn>
2018-07-03 23:12:58 +0800

    thread

---
+++ src/concurrency/Priority.java
@@ -0,0 +1,45 @@
+package concurrency;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 线程优先级没有生效，因为有些操作系统会忽略对线程优先级的设定
+ * 这表示程序正确性不能依赖线程的优先级高低
+ */
+public class Priority {
+    private static volatile boolean notStart=true;
+    private static volatile boolean notEnd=true;
+
+    public static void main(String[] args) throws Exception{
+        List<Job> jobs=new ArrayList<>();
+        for(int i=0;i<10;i++){
+            int priority=i<5?Thread.MIN_PRIORITY:Thread.MAX_PRIORITY;
+            Job job=new Job(priority);
+            jobs.add(job);
+            Thread thread=new Thread(job,"Thread:"+i);
+            thread.setPriority(priority);
+            thread.start();
+        }
+
+    }
+
+    static class Job implements Runnable{
+        private int priority;
+        private long jobCount;
+        public Job(int priority){
+            this.priority=priority;
+        }
+
+        @Override
+        public void run() {
+            while(notStart){
+                Thread.yield();
+            }
+            while (notEnd){
+                Thread.yield();
+                jobCount++;
+            }
+        }
+    }
+}
